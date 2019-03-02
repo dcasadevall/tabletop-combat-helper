9de1671adf552edaf71326d9aefa59b5c518fb05
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-03-02 06:38:08 -0800

    Use WASD movement for panning

--- Assets/Scripts/Prototype/PrototypeCameraController.cs
+++ Assets/Scripts/Prototype/PrototypeCameraController.cs
@@ -23,18 +23,18 @@ namespace Prototype {
 
         private void Update() {
             float speed = 5.0f;
-            if(Input.GetKey(KeyCode.RightArrow)) {
+            if(Input.GetKey(KeyCode.D)) {
                 transform.Translate(new Vector3(speed * Time.deltaTime,0,0));
             }
-            if(Input.GetKey(KeyCode.LeftArrow))
+            if(Input.GetKey(KeyCode.A))
             {
                 transform.Translate(new Vector3(-speed * Time.deltaTime,0,0));
             }
-            if(Input.GetKey(KeyCode.DownArrow))
+            if(Input.GetKey(KeyCode.S))
             {
                 transform.Translate(new Vector3(0,-speed * Time.deltaTime,0));
             }
-            if(Input.GetKey(KeyCode.UpArrow))
+            if(Input.GetKey(KeyCode.W))
             {
                 transform.Translate(new Vector3(0,speed * Time.deltaTime,0));
             }
