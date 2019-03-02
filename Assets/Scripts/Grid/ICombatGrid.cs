3e42c60b99a9448e4b8ddb889c7b263dab044fd0
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-03-01 22:34:52 -0800

    Define CombatGrid and ICombatGrid properties

--- Assets/Scripts/Grid/ICombatGrid.cs
+++ Assets/Scripts/Grid/ICombatGrid.cs
@@ -1,4 +1,6 @@
 ﻿
+using UnityEngine;
+
 namespace Grid {
     /// <summary>
     /// A 2D Grid used to position <see cref="Units.IUnit"/> elements.
@@ -6,7 +8,32 @@ namespace Grid {
     /// Consumers can use this interface to check a <see cref="Units.IUnit"/>s position,
     /// check line of sight between <see cref="Units.IUnit"/>s and move <see cref="Units.IUnit"/>s from
     /// one position to another.
+    ///
+    /// This grid is assumed to be rectangular and of square tile size.
+    /// X and Y axes are assumed to be oriented left to right and bottom to top.
+    /// No rotation is supported for now.
     /// </summary>
     public interface ICombatGrid {
+        /// <summary>
+        /// The number of tiles in the X axis.
+        /// NumTilesX will always be >= 1.
+        /// </summary>
+        uint NumTilesX { get; }
+        /// <summary>
+        /// The number of tiles in the Y axis.
+        /// NumTilesY will always be >= 1.
+        /// </summary>
+        uint NumTilesY { get; }
+        /// <summary>
+        /// How many units (in Unity, 1 unit is 1 meter) a tile measures.
+        /// We intentionally made this an integer to avoid floating point errors and for simplicity.
+        /// 
+        /// TileSize will always be >=1.
+        /// </summary>
+        uint TileSize { get; }
+        /// <summary>
+        /// The origin (0, 0) position in Unity World Space coordinates.
+        /// </summary>
+        Vector2 WorldSpaceOrigin { get; }
     }
 }
