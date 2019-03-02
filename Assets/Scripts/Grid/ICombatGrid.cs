3d4a5a393a90411d838649f79fe884282b59e3d7
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-03-01 21:56:10 -0800

    Initial network setup + Grid interfaces

---
+++ Assets/Scripts/Grid/ICombatGrid.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Grid {
+    /// <summary>
+    /// A 2D Grid used to position <see cref="Units.IUnit"/> elements.
+    /// 
+    /// Consumers can use this interface to check a <see cref="Units.IUnit"/>s position,
+    /// check line of sight between <see cref="Units.IUnit"/>s and move <see cref="Units.IUnit"/>s from
+    /// one position to another.
+    /// </summary>
+    public interface ICombatGrid {
+    }
+}
