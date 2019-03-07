01255bacb8eaa1640511beab5ea8671f8754fab0
Alberto Romero Garcia <nacapa.tgn@gmail.com>
2019-03-07 15:48:15 -0800

    Implement Spiral Pattern Position algorithm

--- Assets/Scripts/Grid/Positioning/SpiralSequenceRandomPositionProvider.cs
+++ Assets/Scripts/Grid/Positioning/SpiralSequenceRandomPositionProvider.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Numerics;
 using Math.Random;
 using UnityEngine;
@@ -14,7 +15,7 @@ namespace Grid.Positioning
     {
         private IRandomProvider _randomProvider;
         private IGridPositionCalculator _gridPositionCalculator;
-        private const float CellPickProbabilityRange = .5f;
+        private const float TileChooseProbability = .3f;
         private const int MaxTries = 100;
 
         public SpiralSequenceRandomPositionProvider(IGridPositionCalculator gridPositionCalculator, 
@@ -28,21 +29,91 @@ namespace Grid.Positioning
         {
             //Test algorithm
             Vector2[] tiles = new Vector2[numTilesToGenerate];
-            Vector2 centerPosition = _gridPositionCalculator.GetTileClosestToCenter(grid);
+            Vector2 spiralCenter = _gridPositionCalculator.GetTileClosestToCenter(grid);
+            Vector2 spiralEndTile = new Vector2(spiralCenter.x - numTilesToGenerate, 
+                                                spiralCenter.y - numTilesToGenerate);
+            LinkedList<Vector2> availableTiles = new LinkedList<Vector2>();
             
-            Debug.Log("Grid Center Tile: (" + (int)centerPosition.x + "," + 
-                      (int)centerPosition.y + ")");
+            Debug.Log("Spiral Center Tile: (" + (int)spiralCenter.x + "," + 
+                      (int)spiralCenter.y + ")");
 
-            for (int i = 0; i < numTilesToGenerate; i++)
-            {
-                int xPosition = (int)centerPosition.x;
-                int yPosition = (int) centerPosition.y + i;
-                tiles[i] = new Vector2(xPosition, yPosition);
+            int k = 1;
+            Vector2 currentPosition, stretchStart = spiralCenter;
+
+            do {
+                currentPosition = new Vector2(stretchStart.x, stretchStart.y);
+                for (int i = (int) stretchStart.y; i > stretchStart.y - k; i--) {
+                    currentPosition.y = i;
+                    AddIfAvailableTile(availableTiles, grid, currentPosition);
+                }
                 
-                Debug.Log("Tile (" + xPosition + "," + yPosition + ") Chosen");
+                //If at this point the position is (center.x - maxDistance, center.y - maxDistance) the spiral is
+                //complete for maxDistance.
+                if (currentPosition == spiralEndTile) break;
+
+                stretchStart = new Vector2(currentPosition.x, currentPosition.y - 1);
+
+                currentPosition = new Vector2(stretchStart.x, stretchStart.y);
+                for (int i = (int) stretchStart.x; i < stretchStart.x + k; i++) {
+                    currentPosition.x = i;
+                    AddIfAvailableTile(availableTiles, grid, currentPosition);
+                }
+
+                stretchStart = new Vector2(currentPosition.x + 1, currentPosition.y);
+                k++;
+
+                currentPosition = new Vector2(stretchStart.x, stretchStart.y);
+                for (int i = (int) stretchStart.y; i < stretchStart.y + k; i++) {
+                    currentPosition.y = i;
+                    AddIfAvailableTile(availableTiles, grid, currentPosition);
+                }
+
+                stretchStart = new Vector2(currentPosition.x, currentPosition.y + 1);
+
+                currentPosition = new Vector2(stretchStart.x, stretchStart.y);
+                for (int i = (int) stretchStart.x; i > stretchStart.x - k; i--) {
+                    currentPosition.x = i;
+                    AddIfAvailableTile(availableTiles, grid, currentPosition);
+                }
+
+                stretchStart = new Vector2(currentPosition.x - 1, currentPosition.y);
+                k++;
+            } while (true);
+
+            return ChooseRandomPositionsFromAvailableTiles(availableTiles, numTilesToGenerate);
+        }
+
+        private void AddIfAvailableTile(LinkedList<Vector2> availableTiles, IGrid grid, Vector2 tile) {
+            if (grid.TileBounds.Contains(tile)) {
+                availableTiles.AddLast(tile);
+                Debug.Log("Tile (" + tile.x + "," + tile.y + ") Chosen");
+            }
+        }
+
+        private Vector2[] ChooseRandomPositionsFromAvailableTiles(LinkedList<Vector2> availableTiles, 
+                                                                  int numTilesToGenerate) {
+
+            Vector2[] tiles = new Vector2[numTilesToGenerate];
+            int chosenTiles = 0;
+            float randomNumber;
+            
+            for (int i = 0; i < MaxTries; i++) {
+                if (chosenTiles == numTilesToGenerate) break;
+                
+                foreach (var tile in availableTiles) {
+                    randomNumber = _randomProvider.GetRandomFloatInRange(0, 1);
+                    if (randomNumber <= TileChooseProbability) {
+                        tiles[chosenTiles] = tile;
+                        chosenTiles++;
+                        availableTiles.Remove(tile);
+                        break;
+                    }
+                }
             }
 
             return tiles;
         }
+
+       
     }
 }
