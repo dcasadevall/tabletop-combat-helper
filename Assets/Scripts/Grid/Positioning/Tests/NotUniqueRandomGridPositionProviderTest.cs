70347608c5a95ad2596e12e9fe82a3a8932371b0
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-03-08 02:43:30 -0800

    Add tests for grid positioning calculation (just one) as sample

--- Assets/Scripts/Grid/Positioning/Tests/NotUniqueRandomGridPositionProviderTest.cs
+++ Assets/Scripts/Grid/Positioning/Tests/NotUniqueRandomGridPositionProviderTest.cs
@@ -1,21 +1,50 @@
+using Math.Random;
+using NSubstitute;
 using NUnit.Framework;
+using UnityEngine;
 using Zenject;
 
 namespace Grid.Positioning.Tests {
     [TestFixture]
     internal class NotUniqueRandomGridPositionProviderTest : ZenjectUnitTestFixture {
+        [Inject]
+        private NotUniqueRandomGridPositionProvider _randomGridPositionProvider;
+
+        private IGrid _grid;
+        private IGridPositionCalculator _gridPositionCalculator;
+        private AlwaysReturnSameWeightRandomProvider _randomProvider;
+
         [SetUp]
         public void CommonInstall() {
+            _grid = Substitute.For<IGrid>();
+            _grid.NumTilesX.Returns((uint)100);
+            _grid.NumTilesY.Returns((uint)100);
+            _grid.TileSize.Returns((uint)1);
+            _gridPositionCalculator = Substitute.For<IGridPositionCalculator>();
+            _randomProvider = new AlwaysReturnSameWeightRandomProvider(0);
+            
+            Container.Bind<IGridPositionCalculator>().To<IGridPositionCalculator>()
+                     .FromInstance(_gridPositionCalculator).AsTransient();
+            Container.Bind<IRandomProvider>().To<AlwaysReturnSameWeightRandomProvider>().FromInstance(_randomProvider)
+                     .AsSingle();
             Container.Bind<NotUniqueRandomGridPositionProvider>().AsSingle();
             Container.Inject(this);
         }
         
-        [Inject]
-        private NotUniqueRandomGridPositionProvider _randomGridPositionProvider;
+        [TestCase(0, 5, 5)]
+        [TestCase(5, 2, 7)]
+        [TestCase(5, -10, 0)]
+        [TestCase(0, 0, 0)]
+        public void testGivenRandomPosition_GetsCenterPosition(int center, int selectedDistanceFromCenter, int expectedPosition) {
+            Vector2 centerPosition = new Vector2(center, center);
+            _randomProvider.SetReturnedWeight(selectedDistanceFromCenter);
+            _gridPositionCalculator.GetTileClosestToCenter(_grid)
+                                   .Returns(centerPosition);
 
-        [TestCase(-5.02, -5)]
-        public void testNoDistanceFromCenter_GetsCenterPosition() {
-            
+            Vector2[] positions =
+                _randomGridPositionProvider.GetRandomUniquePositions(_grid, selectedDistanceFromCenter, 1);
+            Assert.AreEqual(Vector2.one * expectedPosition,
+                            positions[0]);
         }
     }
 }
