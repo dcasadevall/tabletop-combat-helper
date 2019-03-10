379704a00976b912c4da9acf25bca8add9bc3279
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-03-10 16:46:52 -0700

    Add origin world position in GridData. Grid can now be centered at world 0, 0, or have a custom origin world position.

---
+++ Assets/Scripts/Grid/Positioning/Tests/GridPositionCalculatorTest.cs
@@ -0,0 +1,46 @@
+using Math;
+using NSubstitute;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace Grid.Positioning.Tests {
+    [TestFixture]
+    public class GridPositionCalculatorTest {
+        [TestCase(4U, 4U, 3.99f, 3.99f, 3, 3)]
+        [TestCase(4U, 4U, 0.01f, 0.01f, 0, 0)]
+        [TestCase(1U, 1U, 0.99f, 0.99f, 0, 0)]
+        [TestCase(1U, 1U, 0.01f, 0.01f, 0, 0)]
+        [TestCase(63U, 23U, 5.0001f, 22.5f, 5, 22)]
+        [TestCase(2U, 2U, 1.00001f, 1.0001f, 1, 1)]
+        [TestCase(2U, 2U, 1.0f, 2.0f, 1, 1)] // 1.0 and 2.0f considered inside the 1 tile
+        public void TestGivenTileInGrid_GetTileContainingWorldPosition_ReturnsCoordinate(
+            uint numTilesX, uint numTilesY, float x, float y, int expectedCoordX, int expectedCoordY) {
+            IGrid grid = Substitute.For<IGrid>();
+            grid.NumTilesX.Returns(numTilesX);
+            grid.NumTilesY.Returns(numTilesY);
+            grid.TileSize.Returns((uint)1);
+            grid.OriginWorldPosition.Returns(new Vector2(0, 0));
+            
+            IGridPositionCalculator gridPositionCalculator = new GridPositionCalculator();
+            IntVector2? position = gridPositionCalculator.GetTileContainingWorldPosition(grid, new Vector2(x, y));
+            Assert.NotNull(position);
+            Assert.AreEqual(IntVector2.Of(expectedCoordX, expectedCoordY), position);
+        }
+        
+        [TestCase(4U, 4U, 4.01f, 4.01f)]
+        [TestCase(4U, 4U, -0.01f, -0.01f)]
+        [TestCase(1U, 1U, 1.01f, 1.01f)]
+        [TestCase(1U, 1U, -0.01f, -0.01f)]
+        [TestCase(63U, 23U, -5.0f, 23.5f)]
+        [TestCase(2U, 2U, 2.0001f, 2.00001f)]
+        public void TestGivenTileNotInGrid_GetTileContainingWorldPosition_ReturnsNull(uint numTilesX, uint numTilesY, float x, float y) {
+            IGrid grid = Substitute.For<IGrid>();
+            grid.NumTilesX.Returns(numTilesX);
+            grid.NumTilesY.Returns(numTilesY);
+            grid.TileSize.Returns((uint)1);
+            
+            IGridPositionCalculator gridPositionCalculator = new GridPositionCalculator();
+            Assert.IsNull(gridPositionCalculator.GetTileContainingWorldPosition(grid, new Vector2(x, y)));
+        }
+    }
+}
--- Assets/Scripts/Grid/Serialized/IGridData.cs
+++ Assets/Scripts/Grid/Serialized/IGridData.cs
@@ -1,6 +1,21 @@
+
+using UnityEngine;
+
 namespace Grid.Serialized {
     public interface IGridData {
+        /// <summary>
+        /// Number of tiles in the X axis.
+        /// </summary>
         uint NumTilesX { get; }
+        /// <summary>
+        /// Number of tiles in the Y axis.
+        /// </summary>
         uint NumTilesY { get; }
+        /// <summary>
+        /// If specified, the grid's origin will be set to this world position.
+        /// If not specified, the grid's origin will be (-NumTilesX / 2.0f, -NumTilesY / 2.0f).
+        /// e.g: The grid will have it's Center at 0, 0 by default.
+        /// </summary>
+        Vector2? OriginWorldPosition { get; }
     }
 }
