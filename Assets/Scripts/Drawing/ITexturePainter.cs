ea049313da52306213af29858d061636540dcfd9
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-04-08 03:30:49 -0700

    Document things.

--- Assets/Scripts/Drawing/ITexturePainter.cs
+++ Assets/Scripts/Drawing/ITexturePainter.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 namespace Drawing {
+    /// <summary>
+    /// Implementors of this interface will handle painting over a readWrite enabled Sprite.
+    /// </summary>
     public interface ITexturePainter {
         void PaintPixel(Sprite sprite, Vector2 pixel, TexturePaintParams paintParams);
         void EraseAllPixels(Sprite sprite);
