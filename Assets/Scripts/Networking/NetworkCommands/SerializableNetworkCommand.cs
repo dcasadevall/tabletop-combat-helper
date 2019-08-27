02ba70f69d54e38521e6e206c9d960e9d7b54b7e
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-08-27 01:20:42 -0700

    Deserialize network command.

--- Assets/Scripts/Networking/NetworkCommands/SerializableNetworkCommand.cs
+++ Assets/Scripts/Networking/NetworkCommands/SerializableNetworkCommand.cs
@@ -5,23 +5,23 @@ using Replays.Persistence;
 namespace Networking.NetworkCommands {
     [Serializable]
     public class SerializableNetworkCommand : ISerializable {
-        private readonly uint _sequenceIndex;
-        private SerializableCommand _serializableCommand;
+        public readonly uint sequenceIndex;
+        public SerializableCommand command;
 
         public SerializableNetworkCommand(uint sequenceIndex, SerializableCommand command) {
-            _sequenceIndex = sequenceIndex;
-            _serializableCommand = command;
+            this.command = command;
+            this.sequenceIndex = sequenceIndex;
         }
         
         #region ISerializable
         public SerializableNetworkCommand(SerializationInfo info, StreamingContext context) {
-            _sequenceIndex = info.GetUInt32("sequenceIndex");
-            _serializableCommand = (SerializableCommand)info.GetValue("command", typeof(SerializableCommand));
+            sequenceIndex = info.GetUInt32("sequenceIndex");
+            command = (SerializableCommand)info.GetValue("command", typeof(SerializableCommand));
         }
         
         public void GetObjectData(SerializationInfo info, StreamingContext context) {
-            info.AddValue("sequenceIndex", _sequenceIndex);
-            info.AddValue("command", _serializableCommand);
+            info.AddValue("sequenceIndex", sequenceIndex);
+            info.AddValue("command", command);
         }
         #endregion  
     }
