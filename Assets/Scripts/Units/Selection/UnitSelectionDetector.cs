672e09e743b7444a08a86a2e3d13b92d512cf058
Daniel Casadevall Pino <dpino@nianticlabs.com>
2019-12-30 11:22:24 +0100

    Fix mouse drag

--- Assets/Scripts/Units/Selection/UnitSelectionDetector.cs
+++ Assets/Scripts/Units/Selection/UnitSelectionDetector.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Grid;
 using InputSystem;
 using Logging;
@@ -68,27 +69,27 @@ namespace Units.UI {
                                              // And only those clicks which have been held and released within the same unit.
                                              .Where(x => x.Current.Value.Item2 == x.Previous.Value.Item2);
 
+            // Short Click
             var shortClickStream = clickStream.Where(x => x.Current.Interval <= TimeSpan.FromMilliseconds(500));
-            var mouseHeldStream = Observable.EveryUpdate()
-                                            .Where(_ => Input.GetMouseButton(0))
+            shortClickStream.Subscribe(next => OnMouseDown(next.Current.Value.Item2)).AddTo(_disposables);
+
+            // Drag
+            var mouseDragStream = Observable.EveryUpdate()
                                             .Where(_ => !_inputLock.IsLocked)
+                                            .Where(_ => Input.GetMouseButton(0))
                                             .Where(_ => _gridInputManager.TileAtMousePosition.HasValue)
+                                            .Select(_ => Input.mousePosition)
+                                            .ThrottleFirst(TimeSpan.FromMilliseconds(100))
+                                            .TimeInterval()
+                                            .Pairwise()
+                                            // This avoids long lapses between drags
+                                            .Where(x => x.Current.Interval <= TimeSpan.FromMilliseconds(200))
+                                            // Make sure we have moved at least 50 pixels
+                                            .Where(x => Vector3.Distance(x.Current.Value, x.Previous.Value) > 50.0f)
                                             // ReSharper disable once PossibleInvalidOperationException
                                             .Select(_ => _gridInputManager
-                                                         .TileAtMousePosition.Value)
-                                            .Pairwise()
-                                            // Mouse hold stops once we move tile or mouse up before timer
-                                            // .TakeWhile(x => x.Current == x.Previous)
-                                            // Stop once we have a mouse up.
-                                            .TakeUntil(mouseUpStream)
-                                            // We would use ThrottleLast, but it's not available in UniRx,
-                                            // so we have to use Throttlefirst + Skip(1)
-                                            // This gives us the first mouse down event after 500 ms of mouse down.
-                                            .ThrottleFirst(TimeSpan.FromMilliseconds(500))
-                                            .Skip(1);
-            
-            shortClickStream.Subscribe(next => OnMouseDown(next.Current.Value.Item2)).AddTo(_disposables);
-            mouseHeldStream.Subscribe(next => OnMouseHold(next.Current)).AddTo(_disposables);
+                                                         .TileAtMousePosition.Value);
+            mouseDragStream.Subscribe(OnMouseHold).AddTo(_disposables);
         }
 
         public void Dispose() {
@@ -112,6 +113,10 @@ namespace Units.UI {
             
             IUnit[] units = _gridUnitManager.GetUnitsAtTile(tileCoords);
             if (units.Length == 0) {
+                return;
+            }
+
+            if (_inputLock.IsLocked) {
                 return;
             }
 
