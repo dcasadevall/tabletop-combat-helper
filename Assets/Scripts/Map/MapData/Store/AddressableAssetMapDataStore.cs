43abaadcfe5ba0064a375441c74eb928ad0ae462
Daniel Casadevall Pino <dpino@nianticlabs.com>
2020-01-22 16:08:04 +0100

    Exclude Editor code from datastore

--- Assets/Scripts/Map/MapData/Store/AddressableAssetMapDataStore.cs
+++ Assets/Scripts/Map/MapData/Store/AddressableAssetMapDataStore.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using Logging;
 using UniRx.Async;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine.ResourceManagement.AsyncOperations;
 
 namespace Map.MapData.Store {
+    /// <summary>
+    /// Implementation of <see cref="IMapDataStore"/> that uses addressable assets to Load / Commit data.
+    /// </summary>
     public class AddressableAssetMapDataStore : IMapDataStore {
         private readonly List<AddressableAssetMapReference> _addressableAssetMapReferences;
         private readonly ILogger _logger;
@@ -42,16 +47,23 @@ namespace Map.MapData.Store {
         }
 
         public bool Commit(MapStoreId mapStoreId) {
+            // Note: We could create an EditorDataStore implementation instead, but for this little code, we can
+            // just do it inline.
+#if !UNITY_EDITOR
+            _logger.LogError(LoggedFeature.Assets, "Commit(MapStoreId) called when not on editor.");
+            return false;
+#else
             var mapReference = _addressableAssetMapReferences.Find(x => x.MapStoreId == mapStoreId);
             if (mapReference == null) {
                 var msg = $"Invalid MapId: {mapStoreId}";
                 _logger.LogError(LoggedFeature.Assets, msg);
                 return false;
             }
-            
+
             EditorUtility.SetDirty(mapReference.AssetReference.editorAsset);
             AssetDatabase.SaveAssets();
             return true;
+#endif
         }
     }
 }
