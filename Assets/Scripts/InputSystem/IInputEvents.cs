953165f2bf84fb5a8a1cbf7395562ba24b71a898
Daniel Casadevall Pino <dpino@nianticlabs.com>
2020-01-28 17:52:03 +0100

    Remove unnused events

--- Assets/Scripts/InputSystem/IInputEvents.cs
+++ Assets/Scripts/InputSystem/IInputEvents.cs
@@ -13,31 +13,5 @@ namespace InputSystem {
         /// Stream which received values when the right mouse button is pressed (down and up).
         /// </summary>
         IObservable<Unit> RightMouseClickStream { get; }
-        /// <summary>
-        /// Returns a <see cref="MouseDragEvent{T}"/> with values emitted when the mouse is being dragged, as well
-        /// as when the mouse button is released.
-        /// Applies the given (if any) where statements to filter out the events on click.
-        /// This means that no drag values will be emitted at all if the mouse is not initially clicked matching the
-        /// where predicates.
-        /// The Vector2 position returned is in world coordinates.
-        /// </summary>
-        /// <param name="where"></param>
-        /// <returns></returns>
-        MouseDragEvent<Vector2> GetMouseDragEvent(params Func<Vector2, bool>[] where);
-
-        /// <summary>
-        /// Returns a <see cref="MouseDragEvent{T}"/> with values emitted when the mouse is being dragged, as well
-        /// as when the mouse button is released.
-        /// Applies the given (if any) where statements to filter out the events on click.
-        /// This means that no drag values will be emitted at all if the mouse is not initially clicked matching the
-        /// where predicates.
-        /// Applies a select transform function to all values emitted either on mouse down, mouse up, or drag.
-        /// The Vector2 position returned is in world coordinates.
-        /// </summary>
-        /// <param name="select"></param>
-        /// <param name="where"></param>
-        /// <returns></returns>
-        MouseDragEvent<TReturn> GetMouseDragEvent<TReturn>(Func<Vector2, TReturn> select,
-                                                           params Func<TReturn, bool>[] where);
     }
 }
--- Assets/Scripts/InputSystem/InputEvents.cs
+++ Assets/Scripts/InputSystem/InputEvents.cs
@@ -9,15 +9,10 @@ namespace InputSystem {
     /// Helper class with a set of input related observables.
     /// </summary>
     public class InputEvents : IInputEvents {
-        private const float SELECTION_THRESHOLD_VIEWPORT_SIZE = 0.01f;
-
         public IObservable<Unit> LeftMouseClickStream { get; }
         public IObservable<Unit> RightMouseClickStream { get; }
 
-        private readonly CameraInput _cameraInput;
-
-        public InputEvents(EventSystem eventSystem, CameraInput cameraInput) {
-            _cameraInput = cameraInput;
+        public InputEvents(EventSystem eventSystem) {
             LeftMouseClickStream = GetClickStream(0, _ => !eventSystem.IsPointerOverGameObject());
             RightMouseClickStream = GetClickStream(1, _ => !eventSystem.IsPointerOverGameObject());
         }
@@ -34,55 +29,6 @@ namespace InputSystem {
             }
 
             return mouseDownStream.Select(pos => mouseUpStream).Switch().AsUnitObservable();
-        }
-
-        public MouseDragEvent<Vector2> GetMouseDragEvent(params Func<Vector2, bool>[] where) {
-            return GetMouseDragEvent(x => x, where);
-        }
-
-        public MouseDragEvent<TReturn> GetMouseDragEvent<TReturn>(
-            Func<Vector2, TReturn> select, params Func<TReturn, bool>[] where) {
-            IObservable<TReturn> mouseDownStream = Observable.EveryUpdate()
-                                                             .Where(_ => Input.GetMouseButtonDown(0))
-                                                             .Select(_ => _cameraInput.MouseWorldPosition)
-                                                             .Select(select);
-            foreach (var whereStatement in where) {
-                mouseDownStream = mouseDownStream.Where(whereStatement);
-            }
-
-            IObservable<Vector2> mouseUpStream = Observable.EveryUpdate()
-                                                           .Where(__ => Input.GetMouseButtonUp(0))
-                                                           .Select(_ => _cameraInput.MouseWorldPosition);
-
-            // Return a drag stream for each time we receive a mouse down.
-            // This builds an observable of observables which will only emit as we drag.
-            IObservable<Tuple<Vector2, TReturn>> mouseDragStream = mouseDownStream
-                                                                   .Select(mouseDownValue =>
-                                                                               GetMouseDragStream(mouseDownValue,
-                                                                                                  mouseUpStream))
-                                                                   // Switch "Flattens" the observable and only emits the drag events.
-                                                                   .Switch();
-
-            // Also return a stream that will emit values when the drag is ended.
-            IObservable<Tuple<Vector2, TReturn>> dragEndStream = mouseDragStream
-                                                                 .Select(pos => mouseUpStream).Switch()
-                                                                 .Select(worldPos => Tuple.Create(worldPos,
-                                                                                                  select.Invoke(worldPos)));
-
-            return new MouseDragEvent<TReturn>(mouseDragStream, dragEndStream);
-        }
-
-        private IObservable<Tuple<Vector2, T>> GetMouseDragStream<T>(T mouseDownValue,
-                                                                     IObservable<Vector2> mouseUpStream) {
-            return Observable.EveryUpdate()
-                             .Where(_ => Input.GetMouseButton(0))
-                             .Select(_ => _cameraInput.MouseWorldPosition)
-                             .TakeUntil(mouseUpStream)
-                             .Pairwise()
-                             .Where(mousePos =>
-                                        Vector2.Distance(mousePos.Current, mousePos.Previous) >=
-                                        SELECTION_THRESHOLD_VIEWPORT_SIZE)
-                             .Select(x => Tuple.Create(x.Current, mouseDownValue));
         }
     }
 }
